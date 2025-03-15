231d5f67c71d45b3547bcda88f49940d2bd819b3
Bianca de Souza Ramal <bianca.ramal@gran.com>
2025-03-15 20:40:28 -0300

    feat: Insere o upload de imagem

--- scripts.js
+++ scripts.js
@@ -5,3 +5,36 @@ uploadBtn.addEventListener('click', () => {
   inputUpload.click();
 });
 
+function readContentFile(file) {
+    return new Promise((resolve, reject) => {
+        const reader = new FileReader();
+
+        reader.onload = () => {
+            resolve({url: reader.result, name: file.name});
+        }
+
+        reader.onerror = () => {
+            reject(`Erro na leitura do arquivo ${file.name}`);
+        }
+
+        reader.readAsDataURL(file);
+    });
+};
+
+const imagePrincipal = document.querySelector('.main-image');
+const imageName = document.querySelector('.container-image-name');
+
+inputUpload.addEventListener('change', async (event) => {
+    const file = event.target.files[0];
+
+    if(file){
+        try {
+            const contentFile = await readContentFile(file);
+            imagePrincipal.src = contentFile.url;
+            imageName.textContent = contentFile.name;
+        } catch(error) {
+            console.error('Erro na leitura do arquivo.'); 
+        }
+    }
+});
+
