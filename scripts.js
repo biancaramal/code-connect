e6e85dcdf7051ccf348d83bd4c55eeb59cddf75b
Bianca de Souza Ramal <bianca.ramal@gran.com>
2025-03-15 15:54:52 -0300

    feat: Desenvolve o projeto seguindo o layout definido no Figma

---
+++ scripts.js
@@ -0,0 +1,7 @@
+const uploadBtn = document.getElementById('upload-btn');
+const inputUpload = document.getElementById('image-upload');
+
+uploadBtn.addEventListener('click', () => {
+  inputUpload.click();
+});
+
